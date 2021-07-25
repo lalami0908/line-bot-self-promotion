c0b17f197d08fde025b0b29e2b0f12d1151928fb
lalami0908 <b06705048@ntu.edu.tw>
2021-07-26 00:35:14 +0800

    finish message in msg.json

---
+++ reply-1.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    TextMessage, TextSendMessage, ButtonsTemplate, TemplateSendMessage, MessageTemplateAction, ImageSendMessage, StickerSendMessage
+)
+
+import os
+import sys
+import json
+# Channel Secret
+channel_secret = os.getenv('CHANNEL_SECRET', None)
+# Channel Access Token
+channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+# heroku App name
+appName = os.getenv('HEROKU_APP_NAME', None)
+
+if channel_secret is None:
+    print('Specify CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+with open('./msg.json') as msg_file:
+    msgJson = json.load(msg_file)
+
+
+def createImgUrl(ImgfileName):
+    return 'https://' + appName + '.herokuapp.com' + ImgfileName
+
+
+def buttonsTemplate(reply_token, func_name, msg=None):
+    image_url = createImgUrl(msgJson[func_name + 'ImgUrl'])
+    buttons_template = ButtonsTemplate(title=msgJson[func_name + 'Title'], text=msgJson[func_name+'Text'],
+                                       thumbnail_image_url=image_url,
+                                       actions=[MessageTemplateAction(label=i, text=i) for i in msgJson[func_name+'Button']])
+    template_message = TemplateSendMessage(
+        alt_text=msgJson[func_name+'Title'], template=buttons_template)
+
+    line_bot_api.reply_message(reply_token, template_message)
+
+
+def textsMessage(reply_token, func_name, msg=None):
+    if msg is None:
+        messages = [TextMessage(text=i) for i in msgJson[func_name+'Texts']]
+    else:
+        messages = TextMessage(text=msg)
+
+    if msgJson.get(func_name + 'ImgUrl') != None:
+        image_url = createImgUrl(msgJson[func_name + 'ImgUrl'])
+        image = ImageSendMessage(
+            original_content_url=image_url, preview_image_url=image_url)
+        messages.append(image)
+
+    if msgJson.get(func_name + 'HasSticker') != None:
+        package_id = msgJson[func_name + 'StickerId1']
+        sticker_id = msgJson[func_name + 'StickerId2']
+        sticker = StickerSendMessage(
+            package_id=package_id, sticker_id=sticker_id)
+        messages.append(sticker)
+    line_bot_api.reply_message(reply_token, messages)
